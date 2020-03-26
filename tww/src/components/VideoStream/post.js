3f2f08f8f13b1c1aeebdf1926ada2bd194646e7c
Scott Cardoza <scott@popopshuv.com>
2020-03-25 23:08:27 -0600

    working, needs fun stuff

--- tww/src/components/VideoStream/post.js
+++ tww/src/components/VideoStream/post.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react';
+import './style.css'
+import styles from 'styled-components';
 
 const PostContent = (post) => {
+
     post = post.post
     const [selectedPost, setSelectedPost] = useState(false);
+    const [imgToggle, setImgToggle] = useState(false);
+
+
     return (
         <li key={post.id}>
-            <h2 
+            <img className="post-img" src={post.better_featured_image.source_url} style={imgToggle ? {opacity:1} : {}} />
+            <h2 className="post-h2" 
                 dangerouslySetInnerHTML={{__html: post.title.rendered}} 
-                onClick={() => setSelectedPost(!selectedPost)} 
+                onClick={() => setSelectedPost(!selectedPost)}
+                onMouseEnter={()=>{ setImgToggle(!imgToggle)} }
+                onMouseLeave={()=>{ setImgToggle(!imgToggle)}}
             /> 
-             {selectedPost && <div dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>}
+             {selectedPost && <div className="responsiveVideo" dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>}
         </li>
     );
 }
